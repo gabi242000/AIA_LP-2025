aeaad1b2848437702f78f0d2226fa2f35e19fd60
Nedelcu Cosmin <cosminnedelcu0903@gmail.com>
2025-02-28 21:11:59 +0200

    Add files via upload

---
+++ Grupa1/Nedelcu Cosmin lab2/lab2C++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+//     EXERCITIUL 1
+// int main() {
+//     float nota;
+
+//     std::cout << "Ce nota ai luat? (1-10)\n";
+//     std::cin >> nota;
+
+//     if (nota < 5) {
+//         std::cout << "Restanta!!!";
+//     }
+//     else if (nota >= 5 && nota <= 6) {
+//         std::cout << "Satisfacator";
+//     }
+//     else if (nota >= 7 && nota <= 8) {
+//         std::cout << "Foarte bine";
+//     }
+//     else if (nota >= 9 && nota <= 10) {
+//         std::cout << "Excelent";
+//     }
+//     else {
+//         std::cout << "Nota invalida!";
+//     }
+
+//     return 0;
+// }
+
+
