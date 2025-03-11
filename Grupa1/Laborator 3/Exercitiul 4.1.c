e0461cb91668baf5e582f1f06ed0b514d2dfb9fe
Rosca_Remus_Gabriel <roscaremus606@gmail.com>
2025-03-11 21:24:39 +0200

    Laborator3

--- Grupa1/Laborator 3/Exercitiul 4.1.c
+++ Grupa1/Laborator 3/Exercitiul 4.1.c
@@ -6,8 +6,8 @@ int main(){
     printf("Introdu un numar:\n");
     scanf("%d",&n);
 
-    for(i=1;i<=n;i++)
-        if(i%2==0) printf("%d ",i);
+    for(i=2;i<=n;i+=2)
+        printf("%d ",i);
 
     
 
