9f9ff5db74765eefa3e18cd0042b5293a12122c9
Mirunaelenav26 <miruna.vladoiu05@gmail.com>
2025-03-23 01:02:26 +0200

    Update ex4_1.c

--- Tema_2/ex4_1.c
+++ Tema_2/ex4_1.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    //ex_4.2
-    int N,i,f1,f2,f3;
-    printf("Introdu un numar pozitiv: ");
+    int N,i;
+    printf("Introdu un numar: \n");
     scanf("%d",&N);
-    f1=0;
-    f2=1;
-    printf("Primele numere din sirul lui Fibonacci sunt: ");
-    printf("%d %d\n",f1,f2);
-    for(i=3;i<=N;i++)
+    printf("Numerele pare pana la %d sunt:",N);
+    for(i=0;i<N;i++)
     {
-        f3=f1+f2;
-        f1=f2;
-        f2=f3;
-        printf("%d\n",f3);
+        if(i%2==0)
+        printf("%d ",i);
     }
-return 0;
+    
+    return 0;
 }
