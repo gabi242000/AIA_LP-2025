cebc847ac7242faa5d9a999e0d36e57a56b602d8
Teodor Andrei Plaeasu <30527339+teodorandrei@users.noreply.github.com>
2025-03-24 01:52:00 +0200

    Create CifruCaesar.c

---
+++ Grupa2/Teme/Tema L5/CifruCaesar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char s[100];
+    int p,i;
+    printf("Introdu sirul: ");
+    fgets(s,100,stdin);
+    printf("Introdu p: ");
+    scanf("%d",&p);
+    for (i=0;i<strlen(s)-1;i++){
+        if (isalpha(s[i])){
+            if (islower(s[i]))
+                if (s[i]+p>'z')
+                    s[i]='a'-('z'-s[i]+1)+p;
+                else
+                    s[i]=s[i]+p;
+            if (isupper(s[i]))
+                if (s[i]+p>'Z')
+                    s[i]='A'-('Z'-s[i]+1)+p;
+                else
+                    s[i]=s[i]+p;
+        }
+    }
+    printf("%s",s);
+    return 0;
+}
