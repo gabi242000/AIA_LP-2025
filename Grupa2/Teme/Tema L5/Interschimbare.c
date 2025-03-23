7ac6a1c56e72525ea46e1b41e4a1bb4a4b9e345a
Teodor Andrei Plaeasu <30527339+teodorandrei@users.noreply.github.com>
2025-03-24 01:54:36 +0200

    Create Interschimbare.c

---
+++ Grupa2/Teme/Tema L5/Interschimbare.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char s[100];
+    int i,aux,x,y;
+    printf("Introdu sirul: ");
+    fgets (s,100,stdin);
+    scanf("%d %d",&x ,&y);
+    aux=s[x];
+    s[x]=s[y];
+    s[y]=aux;
+    printf("%s",s);
+    return 0;
+}
