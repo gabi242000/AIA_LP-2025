cd3a695cf4e5f078683f67f00352395dbd27fd25
silviunica05 <silviu.gabriel.nica@gmail.com>
2025-03-26 20:53:40 +0200

    Add files via upload

---
+++ Tema 2 pclp.c
@@ -0,0 +1,38 @@
+// Exercitiul 4.1:
+//#include <stdio.h>
+//
+//int main()
+//{
+//   int N;
+//   printf("Introduceti un numar: ");
+//   scanf("%d", &N);
+//   for(int i=0; i<N; i+=2)
+//      printf("%d ", i);
+// }
+
+// #include <stdio.h>
+
+//Exercitiul 4.2:
+// int main()
+// {
+    // int N, x[100];
+    // printf("Introduceti un numar: ");
+    // scanf("%d", &N);
+    // x[1]=1;
+    // for(int i=2; i<=N; i++){
+        // x[i]=x[i-1]+x[i-2];
+    // printf("%d ", x[i]);
+    // }
+// }
+
+// Exercitiul 4.3:
+// #include <stdio.h>
+
+// int main()
+// {
+    // int N, x[5][5];
+    // printf("Introduceti un numar: ");
+    // scanf("%d", &N);
+    // for(int i=0; i<N; i++)
+    // for(int j=0; j<N; j++)
+// }
