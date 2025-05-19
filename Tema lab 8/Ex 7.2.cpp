3ead31cc0d24082d35caa040b7edd8e5f8622494
Ilincapisica <ilinca.alistar29@gmail.com>
2025-05-19 09:40:18 +0300

    Update Ex 7.2.cpp

--- Tema lab 8/Ex 7.2.cpp
+++ Tema lab 8/Ex 7.2.cpp
@@ -23,7 +23,7 @@ int main() {
         }
     }
 
-    // Afișare matrice
+    // Afisare matrice
     cout << "Matricea identitate:\n";
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
