8292118f1a4a6250cb765506658c5a1064243797
anaRaluca5 <grigoreanaraluca@gmail.com>
2025-05-25 14:28:27 +0300

    Add files via upload

---
+++ Grupa1/ex2_LAB9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int cmmdc(int a, int b) {
+    if(b==0)
+        return a;
+    return cmmdc(b, a % b);
+}
+
+int main() {
+    int a, b;
+    printf("Introdu doua numere: ");
+    scanf("%d %d", &a, &b);
+
+    int rezultat=cmmdc(a, b);
+    printf("CMMD dintre %d si %d este: %d\n", a, b, rezultat);
+
+    return 0;
+}
