ea5174128b9a410ca1e900415702376e90d7d4a4
BesciuDarius <besciudarius@gmail.com>
2025-10-08 16:27:36 +0300

    Add files via upload

---
+++ Java/Tema 1/Fac.java
@@ -0,0 +1,57 @@
+package Fac;
+import java.io.*;
+/*
+public class Fac {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.println("Introduceti un nr natural: ");
+		String s = br.readLine();
+		 try {
+	            int n= Integer.parseInt(s);
+	            if (n>=0) {
+	                System.out.println("nr introdus " + n);
+	                if( n%2==0) 
+	                    System.out.println("nr este par");
+
+	                else System.out.println("nr este impar");
+	            }
+	        }catch (NumberFormatException e) {
+	            System.out.println("valoare introdusa nu este un nr nat valid ma tata ma!!");
+	        }
+	    }
+	}
+*/
+
+/*
+public class Fac {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.println("Introduceti trei nr naturale: ");
+		String s = br.readLine();
+		String n = br.readLine();
+		String m = br.readLine();
+		
+		int a= Integer.parseInt(s);
+		int b= Integer.parseInt(n);
+		int c= Integer.parseInt(m);
+		int suma = 0, x, y;
+		float medie = 0;
+		
+		suma = a + b + c;
+		medie = (a + b + c)/3;
+		x = a; y = a;
+		if (x <= b) x = b;
+		if (x <= c) x = c;
+		if (y >= b) y = b;
+		if (y >= c) y = c;
+		 System.out.println("Suma: " + suma + " , Media: " + medie + " , Cel mai mic nr : " + y + " , Cel mai mare nr: " + x);
+		 
+		}
+	}
+*/
