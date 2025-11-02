99e32e98bfd882baa03a43f5c246fc511e55f3fa
anaRaluca5 <grigoreanaraluca@gmail.com>
2025-11-02 02:48:10 +0200

    Add files via upload

---
+++ Java/aplicatia1.java
@@ -0,0 +1,35 @@
+package temalab1;
+import java.io.*;
+
+public class aplicatia1 {
+
+	public static void main(String[] args)throws IOException {
+		
+		String s;
+		int nr;
+		
+		System.out.print("Introduceti un numar natural: ");
+		InputStreamReader isr= new InputStreamReader(System.in);
+		BufferedReader br= new BufferedReader(isr);
+		s= br.readLine();
+		
+		//verificam daca sirul contine doar cifre
+		if( s.matches("[0-9]+")) { //doar cifre
+			nr= Integer.parseInt(s);
+			System.out.println("Numarul introdus este: "+ nr);
+			
+			if(nr%2==0) 
+				System.out.println("Numarul este par");
+			else
+				System.out.println("Numarul este impar.");
+					
+			}
+		else {
+			System.out.println("Valoarea introdusa nu este valida!");
+			
+				
+			}
+		}
+	}
+
+
---
+++ Java/aplicatia2.java
@@ -0,0 +1,44 @@
+package temalab1;
+import java.io.*;
+
+public class aplicatia2 {
+
+	public static void main(String[] args)throws IOException {
+		int a, b, c, suma;
+		double media;
+		
+		InputStreamReader isr= new InputStreamReader(System.in);
+		BufferedReader br= new BufferedReader(isr);
+		
+		System.out.print("Introduceti primul numar intreg: ");
+		a= Integer.parseInt(br.readLine());
+		
+		System.out.print("Introduceti al doilea numar intreg: ");
+		b= Integer.parseInt(br.readLine());
+		
+		System.out.print("Introduceti al treilea numar intreg: ");
+		c= Integer.parseInt(br.readLine());
+		
+		suma= a+b+c;
+		
+		media=suma/3.0;
+		
+		int maxim= a;
+		if(b>maxim)
+			maxim=b;
+		if(c>maxim)
+			maxim=c;
+		
+		int minim= a;
+		if(b<minim)
+			minim=b;
+		if(c<minim)
+			minim=c;
+		
+		System.out.println("Suma celor trei numere este: " + suma);
+		System.out.println("Media aritmetica a celor trei numere este: " + media);
+		System.out.println("Cel mai mare numar este: "+ maxim);
+		System.out.println("Cel mai mic numar este: " + minim);
+	}
+
+}
